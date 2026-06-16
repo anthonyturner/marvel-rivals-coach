92495c3978bcbccdb1484e487dbb92ad1a99d3e3
anthony turner <anthonyturner2k@gmail.com>
2026-06-16 02:12:49 -0400

    feat: update hero detail modal styles and improve layout; enhance CSS variables for better theming

--- api/heroes.ts
+++ api/heroes.ts
@@ -4,7 +4,5 @@ import { getHeroesFromDatabase } from '../src/content-database.js';
 import { handleJsonRequest } from '../src/vercel-api.js';
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
-  console.log("api heroes hit", req)
-  console.log("api ServerResponse", res)
   return handleJsonRequest(req, res, () => getHeroesFromDatabase());
 }
